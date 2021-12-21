d1610b7eef6220693fcaa4af34c3d9ee0112847a
Nina Velikajne <nv6920@student.uni-lj.si>
2021-12-21 16:03:21 +0100

    Edited openapi url

--- api/src/main/java/si/fri/rso/recommendation/api/v1/RecommendationSystemApplication.java
+++ api/src/main/java/si/fri/rso/recommendation/api/v1/RecommendationSystemApplication.java
@@ -12,11 +12,10 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-
 @SecurityScheme(name = "openid-connect", type = SecuritySchemeType.OPENIDCONNECT,
         openIdConnectUrl = "http://auth-server-url/.well-known/openid-configuration")
 @ApplicationPath("v1")
-@OpenAPIDefinition(info = @Info(title = "CustomerApi", version = "v2.0.0", contact = @Contact(email = "rentarich@gmail.com"), license = @License(name="RentarichLicense")), servers = @Server(url = "http://localhost:3333/v1"), security
+@OpenAPIDefinition(info = @Info(title = "CustomerApi", version = "v2.0.0", contact = @Contact(email = "rentarich@gmail.com"), license = @License(name="RentarichLicense")), servers = @Server(url = "http://20.62.179.11/recommendation"), security
         = @SecurityRequirement(name = "openid-connect"))
 public class RecommendationSystemApplication extends Application {
 }
